26ce62be70c3f12c385a2e8df4c4214cf8f8526d
Cameron Chandler <webstormcam@gmail.com>
2022-03-08 05:51:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var values = [8.00,12.00,16.00,24.00,36.00];
+const values = [8.00,12.00,16.00,24.00,36.00];
 
-var input = document.getElementById('myRange'),
-   output = document.getElementById('output');
+let input = document.getElementById('myRange');
+  let output = document.getElementById('output');
 input.oninput = function(){
     output.innerHTML = values[this.value];
 };

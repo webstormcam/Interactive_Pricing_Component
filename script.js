3200989d2037daec53e95840b89b2713fbd25fc4
Cameron Chandler <webstormcam@gmail.com>
2022-03-08 21:08:35 -0500

    I gotta figure out what numbers are correct lol

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const values = [8.00,12.00,16.00,24.00,36.00];
+const values = [8,12,16,24,36];
+const button = document.getElementById('button')
 
 let input = document.getElementById('myRange');
   let output = document.getElementById('output');
@@ -6,3 +7,12 @@ input.oninput = function(){
     output.innerHTML = values[this.value];
 };
 input.oninput();
+
+button.addEventListener('click',function(){
+ for(let i=0;i<values.length;i++){
+   values[i]*=0.25
+   console.log(values)
+   
+ }
+  input.oninput();
+})
